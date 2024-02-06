45bd5569e5d5639ae1badd5f4f84df3adf0dae9b
Omar-jabra <129175570+Omar-Tood@users.noreply.github.com>
2024-02-06 21:15:55 +0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import { db } from "@/lib/db";
 
 export default async function Home() {
-  const tasks = await db.task.findMany();
+  const tasks = await db.task.findMany({
+    where:{
+      title:{
+        contains: ""
+      }
+    }
+  })
 
   return (
     <div>
